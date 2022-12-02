f2917f842eb2a1e4320db254d4d6c4511debd574
IlluzionzDev <jaminstratford@gmail.com>
2022-12-03 10:01:56 +1300

    Log used host

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,2 +1,3 @@
 // WebSocket host server
-export const HOST = process.env.HOST || 'ws://52.64.253.66:4000';
+export const HOST = process.env.HOST || 'wss://52.64.253.66:4000';
+console.debug('Host is', HOST)
