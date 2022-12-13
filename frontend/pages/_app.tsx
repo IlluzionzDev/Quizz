5fcf3f0f6b9bf850f83bb347cbaa696c8d9fcc3b
IlluzionzDev <jaminstratford@gmail.com>
2022-12-13 15:37:45 +1300

    Add new design system variables

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,14 +3,14 @@ import '@styles/design-system.scss';
 import type { AppProps } from 'next/app';
 import store from '../store/store';
 import { Provider } from 'react-redux';
-import '@fontsource/inter';
-import '@fontsource/lexend-deca'
-import '@fontsource/mulish'
-import '@fontsource/source-sans-pro'
 
-// Normal & Bold
-import '@fontsource/poppins/700.css'
-import '@fontsource/poppins/400.css'
+// Font stlying
+import '@fontsource/poppins/700.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/inter/700.css';
+import '@fontsource/inter/400.css';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
